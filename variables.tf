75afe6227db71a7ebf61f34f208d892a46aa25b9
Vebjørn Gilberg <vebgil@netcompany.com>
2025-02-26 15:01:34 +0100

    add provider.tf and variable.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "gcp_allente_test_SA" {
+  description = "The path to the service account key file for the GCP project"
+  default     = "allente-training-sa.json"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The GCP project ID"
+  default     = "allente-training"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  default     = "europe-west1"
+  type        = string
+}
