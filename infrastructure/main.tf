3c5c1cb0b252f62d97cc8a13e4335d9d6793e3f2
Vebjørn Gilberg <vebgil@netcompany.com>
2025-03-03 10:47:38 +0100

    try automatic

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -97,7 +97,7 @@ resource "google_secret_manager_secret" "cloud_run_sa_key" {
   depends_on  = [google_project_service.secretmanager] # Ensure the Secret Manager API is enabled before creating the secret
   secret_id   = "cloud-run-sa-key"
   replication {
-    auto = true  # Updated from 'automatic' to 'auto'
+    automatic = true  # Updated from 'automatic' to 'auto'
   }
   lifecycle {
     ignore_changes = [secret_id] # Don't recreate the secret if the ID changes
